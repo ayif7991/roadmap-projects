8ff068f5ff7254902b49f475c9eff9bef6cda722
alfiya <alfiya060@gmail.com>
2025-03-26 00:11:57 +0100

    add error handling for city not found in weather API

--- weather API/index.py
+++ weather API/index.py
@@ -6,9 +6,12 @@ api_key = "03d9b86ae11e33a003449755b9989735"
 user_input = input("Enter city name: ")
 weather_data = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
 
-weather = weather_data.json()["weather"][0]["description"]
-temperature = weather_data.json()["main"]["temp"]
-print(f"Temperature: {temperature}°F\nWeather: {weather}")
+if weather_data.status_code != 200:
+    print("City not found")
+else:
+    weather = weather_data.json()["weather"][0]["description"]
+    temperature = weather_data.json()["main"]["temp"]
+    print(f"Temperature: {temperature}°F\nWeather: {weather}")
 
 
 
